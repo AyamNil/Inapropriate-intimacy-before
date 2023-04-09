246890e363095339c5216aa9b72ed534a94f06b9
Risya Emha Abdillah <87582509+AyamNil@users.noreply.github.com>
2023-04-10 01:50:50 +0700

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,25 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private Customer owner;
+
+    public BankAccount(String accountNumber, double balance, Customer owner) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void displayAccountInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: $" + balance);
+        System.out.println("Owner: " + owner.getName());
+    }
+}
---
+++ src/Customer.java
@@ -0,0 +1,17 @@
+public class Customer {
+        private String name;
+        private String address;
+    
+        public Customer(String name, String address) {
+            this.name = name;
+            this.address = address;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public String getAddress() {
+            return address;
+        }
+}
